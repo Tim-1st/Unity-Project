0061794612f1b6e9c3e9c7f15f8f46773a9db15a
Timéo <timeodc@icloud.com>
2026-03-12 09:39:16 +0100

    update

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
 using TMPro;
 using System.Collections;
+using UnityEngine.Rendering;
 
 
 public class PlayerHealth : MonoBehaviour
 {
+    [SerializeField] 
+    private SpriteRenderer sr;
+
+    [SerializeField]
+    private HealthBar healthBar;
 
     [SerializeField] 
     private int currentLifePoints;
@@ -22,6 +28,7 @@ public class PlayerHealth : MonoBehaviour
     {
         currentLifePoints = maxLifePoints;
         currentLifePointsText.SetText(currentLifePoints.ToString());
+        healthBar.SetHealth((float)currentLifePoints / maxLifePoints);
     }
 
     public void TakeDamage(int damage = 1)
@@ -35,6 +42,8 @@ public class PlayerHealth : MonoBehaviour
         currentLifePoints = Mathf.Clamp(currentLifePoints - damage, 0, maxLifePoints);
         currentLifePointsText.SetText(currentLifePoints.ToString());
 
+        healthBar.SetHealth((float)currentLifePoints / maxLifePoints);
+
         if (currentLifePoints == 0)
         {
            Debug.Log("Game Over");
@@ -44,17 +53,42 @@ public class PlayerHealth : MonoBehaviour
     IEnumerator InvulnerableDuration()
     {
        
-        
-        float duration = 5f;
+        isInvulnerable = true;
+
+        float duration = 1.25f;
         float timeElasped = 0f;
 
-        yield return new WaitForSeconds(duration);
+        float flashDuration = 0.2f;
+        float flashTimeElasped = 0f;
 
-        //while (timeElasped < duration)
-        //{
-       //     timeElasped += Time.deltaTime;
-      //      yield return null;
-      //  }
+        bool isVisible = true;
+
+
+        while (timeElasped < duration)
+        {
+            timeElasped += Time.deltaTime;
+            flashTimeElasped += Time.deltaTime;
+
+            if (flashTimeElasped >= flashDuration)
+            {
+                if (isVisible)
+                {
+                    sr.color = Color.clear;
+                } 
+                else
+                {
+                    sr.color = Color.white;
+                }
+
+                flashTimeElasped = 0f;
+                isVisible = !isVisible;
+            }
+
+           yield return null;
+        }
+
+        sr.color = Color.white;
+
         isInvulnerable = false;
     }
 
